eafb4053304a61f857986c75ed7d321dfedd79a9
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-05-26 12:05:40 -0300

    borrado de otros pis

--- api/src/routes/genres.js
+++
@@ -1,8 +0,0 @@
-const {Router}= require ('express');
-const router = Router();
-const {getGenres} = require ('../controllers/genres');
-
-router.get('/',getGenres)
-
-module.exports =router;
-
--- api/src/routes/platforms.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require('express');
-const router= Router();
-const {getPlatforms} = require('../controllers/platforms')
-
-router.get('/', getPlatforms)
-
-module.exports =router;
